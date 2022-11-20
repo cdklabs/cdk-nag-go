3be5ed8795f27d8665226c17a68895fcdf3f6fd0
cdklabs-automation <cdklabs-automation@amazon.com>
2022-11-20 00:25:24 +0000

    chore(release): v2.21.7

--- cdknag/jsii/jsii.go
+++ cdknag/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed cdk-nag-2.21.6.tgz
+//go:embed cdk-nag-2.21.7.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdk-nag", "2.21.6", tarball)
+	_jsii_.Load("cdk-nag", "2.21.7", tarball)
 }
